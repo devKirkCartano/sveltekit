dfae7734e5b97ce3c15f9e1a81b0a0aac1c2008a
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2024-07-04 12:33:24 +0800

    add dynamic routes

---
+++ src/routes/product/+page.svelte
@@ -0,0 +1,6 @@
+<h1>Product List</h1>
+<p>product 1</p>
+<p>product 2</p>
+<p>product 3</p>
+<p>product 4</p>
+<p>product 5</p>
---
+++ src/routes/product/[productId]/edit/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    import {page} from '$app/stores';
+
+    const productId = $page.params.productId;
+</script>
+
+<h1>Edit Product {productId}</h1>
