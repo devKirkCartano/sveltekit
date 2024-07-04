eef5fc8975f8ad21d8805fe44722d249fd300217
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2024-07-04 13:04:53 +0800

    add catch all routes

---
+++ src/routes/docs/[...slug]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import {page} from '$app/stores';
+
+    const slug = $page.params.slug;
+    const slugArr = $page.params.slug.split('/');
+    console.log(slugArr);
+</script>
+
+<h1>Docs Home Page</h1>
+<p>Slug: {slug}</p>
+
+{#if slugArr.length === 1}
+    <p>Viewing docs for feature: {slugArr[0]}</p>
+{:else if slugArr.length === 2}
+    <p>Viewing docs for feature {slugArr[0] } and concept {slugArr[1]}</p>
+{/if}
