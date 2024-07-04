d00db4ea22fde9c8207d3502fc81f2e596f60ec6
Kirk Cedrick S. Cartaño <97022622+devKirkCartano@users.noreply.github.com>
2024-07-04 13:53:11 +0800

    navigate programmatically

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,22 @@
+<script>
+    import {goto, beforeNavigate, afterNavigate} from '$app/navigation';
+
+    const handleClick = () => {
+        console.log('Placing your order')
+        goto('/product');
+    }
+    beforeNavigate((navigation) => {
+        console.log({before: navigation})
+    })
+    afterNavigate((navigation) => {
+        console.log({after: navigation})
+    })
+</script>
+
+
 <h1>Welcome Home</h1>
 <a href="/blog">Blog</a>
 <a href="/docs">Docs</a>
 <a href="/product">Product</a>
 <a href="/marketing">Marketing</a>
+<button on:click={handleClick}>Place order</button>
